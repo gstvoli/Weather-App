365912e2ee431e728f80c0372bb8b4e39572d1a7
Gustavo Oliveira <guga.oli.1357@gmail.com>
2023-04-19 12:36:30 -0300

    stash test?

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8º</Text>
+          <Text style={styles.highLow}>Low: 6 º</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
+        <Text></Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  temp: {
+    color: '#000',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: '#000'
+  },
+  highLow: {
+    color: '#000',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
+})
+
+export default App
