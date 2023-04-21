6e928da5177cae1c3ffea4886834157d34e3f546
Gustavo Oliveira <guga.oli.1357@gmail.com>
2023-04-21 12:02:29 -0300

    Creation of upcoming weather screen and some components

--- App.js
+++ App.js
@@ -1,60 +1,19 @@
-import { Text, View, StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import CurrentWeather from './src/components/CurrentWeather'
+import UpComingWeather from './src/components/UpcomingWeather'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text>Current Weather</Text>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8º</Text>
-          <Text style={styles.highLow}>Low: 6 º</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Its sunny</Text>
-        <Text style={styles.message}>Its perfect t-shirt weather</Text>
-        <Text></Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <UpComingWeather />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    backgroundColor: 'pink'
-  },
   container: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  temp: {
-    color: '#000',
-    fontSize: 48
-  },
-  feels: {
-    fontSize: 30,
-    color: '#000'
-  },
-  highLow: {
-    color: '#000',
-    fontSize: 20
-  },
-  highLowWrapper: {
-    flexDirection: 'row'
-  },
-  bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start'
-  },
-  description: {
-    fontSize: 48
-  },
-  message: {
-    fontSize: 30
+    flex: 1
   }
 })
 
