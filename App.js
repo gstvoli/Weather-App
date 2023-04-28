169c18ce8445e46797ee22e77b9fac974bfdffcc
Gustavo Oliveira <guga.oli.1357@gmail.com>
2023-04-28 12:50:40 -0300

    Getting the weather data and fetching it

--- App.js
+++ App.js
@@ -4,10 +4,28 @@ import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
 import * as Location from 'expo-location'
 
+import { WEATHER_API_KEY } from '@env'
+
 const App = () => {
   const [loading, setLoading] = useState(true)
-  const [location, setLocation] = useState(null)
   const [error, setError] = useState(null)
+  const [weather, setWeather] = useState([])
+  const [lat, setLat] = useState([])
+  const [lon, setLon] = useState([])
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+      )
+      const data = await res.json()
+      setWeather(data)
+    } catch (e) {
+      setError('Could not fetch Weather')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
     ;(async () => {
@@ -18,10 +36,15 @@ const App = () => {
         return
       }
       let location = await Location.getCurrentPositionAsync({})
-      console.log(location)
-      setLocation(location)
+      setLat(location.coords.latitude)
+      setLon(location.coords.longitude)
+      await fetchWeatherData()
     })()
-  }, [])
+  }, [lat, lon])
+
+  if (weather) {
+    console.log(weather)
+  }
 
   if (loading) {
     return (
